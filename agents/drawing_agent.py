565fe8d5f48f74bff16609caa85624ef6a5489b9
bobo <danknessdra@gmail.com>
2024-04-20 17:42:08 -0700

    agents need api

--- agents/drawing_agent.py
+++ agents/drawing_agent.py
@@ -1,9 +1,20 @@
 from uagents import Agent, Context, Model
 import base64
-GUESSING_AGENT_ADDRESS = "agent1qw23p2euxrt0ysppyfaxn46gusswu3tm2jrtgc5xq4kh328u2r7ej555mcc"
-class Request(Model):
-    correct: str
+import requests
+import os
+import together
+together.api_key="713ab4d86fc2505d5632f1cf7039fe3740bd25c15d56722a4e94f5067eb81c11"
+def get_ai_image(correct):
+    response = together.Image.create(
+        prompt=f"{correct} drawn in crude pictionary style by a child in black on a white",
+        model="stabilityai/stable-diffusion-2-1", width=500, height=500)
+    image = response["output"]["choices"][0]
+    output = image["image_base64"]
+    return output
 
+GUESSING_AGENT_ADDRESS = "agent1qw23p2euxrt0ysppyfaxn46gusswu3tm2jrtgc5xq4kh328u2r7ej555mcc"
+class Request0(Model):
+    correct: str
 
 class Response(Model):
     text: str
@@ -25,19 +36,14 @@ async def startup(ctx: Context):
     ctx.logger.info(f"With address: {drawing_agent.address}")
 
 
-@drawing_agent.on_query(model=Request, replies={Response, GuesserRequest})
-async def query_handler(ctx: Context, sender: str, _query: Request):
+@drawing_agent.on_query(model=Request0, replies={Response, GuesserRequest, Request0})
+async def query_handler(ctx: Context, sender: str, _query: Request0):
     ctx.logger.info("Query received")
-    try:
-        # base64 encode shit here
 
-        
-        
-        res = await ctx.send(GUESSING_AGENT_ADDRESS, Request())
-        await ctx.send(sender, Request(text=res))
-    except Exception:
-        
-        await ctx.send(sender, Response(text="fail"))
+    # img = get_ai_image(_query.correct)
+
+    
+    await ctx.send(sender, Request0(correct="iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFUlEQVR42mP8z8BQz0AEYBxVSF+FABJADveWkH6oAAAAAElFTkSuQmCC"))
 
 
 if __name__ == "__main__":
